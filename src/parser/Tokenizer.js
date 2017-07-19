b86d91348163482dee03659773bfcadd75d36636
Jake Chitel <jchitel@gmail.com>
2017-07-19 14:05:30 -0500

    Decided on good logic for Tokenizer, started reimplementing

--- src/parser/Tokenizer.js
+++ src/parser/Tokenizer.js
@@ -89,140 +89,204 @@ export default class Tokenizer {
         this.offset = 0;
     }
 
+    /**
+     * These are the rules for the first character:
+     * - lowercase letter: keyword
+     *   - following characters should exactly match a keyword, followed by any non-identifier character to bound the token.
+     *   - any failure to match the above specification should fall back to identifier
+     * - letter or underscore: identifier
+     *   - continue to match valid identifier characters until a non-identifier character to bound the token.
+     * - dash: numeric literal
+     *   - if followed by number, follow numeric rules below
+     *   - otherwise, take as operator
+     * - 0: zero, non-decimal integer, or floating point
+     *   - if the following is not [XxEeBb.0-9], then it is 0.
+     *   - if it is [Xx], it is a hexadecimal literal. consume [a-fA-F0-9]+ as the remainder of the token.
+     *   - if it is [Bb], it is a binary literal. consume [01]+ as the remainder of the token.
+     *   - if it is [0-9], it is a decimal (integer or floating) literal. consume [0-9]+
+     *     - if next is [.], it is a floating literal. consume [0-9]+
+     *       - if next is [Ee], consume [0-9]+ as the remainder of the token.
+     *       - anything else, and the token is done.
+     *   - if it is [.], it is a floating literal. consume [0-9]+
+     *     - if next is [Ee], consume [0-9]+ as the remainder of the token.
+     *     - anything else, and the token is done.
+     *   - if next is [Ee], floating point. consume [0-9]+ as the remainder of the token.
+     *   - anything else, token is just 0.
+     * - [1-9]: numeric literal
+     *   - consume [0-9]*
+     *     - if next is [.], it is floating point. consume [0-9]+
+     *       - if next is [Ee], consume [0-9]+ as the remainder of the token.
+     *       - anything else, token is done.
+     *     - if next is [Ee], it is floating point. consume [0-9]+ as the remainder of the token.
+     *     - anything else, decimal integer literal
+     * - ": string literal
+     *   - consume anything but a " and a \.
+     *   - if \, then consume escape sequence.
+     *     - if [nrtvfb], insert these special escape characters.
+     *     - if [Xx], insert ascii code
+     *       - get [0-9A-Fa-f]{2}, parse code to character
+     *     - if [Uu], insert unicode
+     *       - if '{', check next 5 or 6 characters, should be [0-9A-Fa-f], followed by '}', parse code to character
+     *       - otherwise, should be [0-9A-Fa-f]{4}, parse code to character
+     *     - anything else, just take the character as it is
+     *   - stop immediately at next ".
+     * - ': character literal
+     *   - consume JUST 1 of anything but a ' and a \.
+     *   - if \, follow same rules as string above.
+     *   - next character must be a '.
+     * - [:{}()[],;`.=]: symbol
+     *   - all are single characters except =.
+     *   - if =
+     *     - if >, take as fat_arrow
+     *     - any other operator character, take as operator.
+     * - [~!$%^&*+-|<>?/']: operator
+     *   - consume any combination of operator characters, including =. (operators starting with = are handled above)
+     * - (\r\n|\n|;): new line
+     *   - any of these register as a new line character, which is a special class of whitespace (some expansions require being ended by a new line)
+     *   - \r not followed by \n is regular whitespace
+     * - [ \r\t]: whitespace
+     *   - any combination of these characters registers as whitespace
+     * - anything else is an invalid character (for now)
+     *
+     * So these are the lookahead requirements:
+     * - keyword: 1
+     * - identifier: 1
+     * - integer literal: 3 (extra for -, 0x, 0b)
+     * - floating point literal: 2 (extra for ., e)
+     * - string literal: 10 (extra for \, \x, \u, \u{......})
+     * - character literal: 10 (see above)
+     * - symbol: 3 (extra for =>)
+     * - operator: 1
+     * - new line: 2 (extra for \r)
+     * - whitespace: 1
+     */
     *[Symbol.iterator]() {
-        let integerLiteral = '',
-            floatLiteral = '',
-            stringLiteral = '',
-            charLiteral = '',
-            symbol = '',
-            newLine = '',
-            whitespace = '';
+        let image = '';       // progressive image of current token being analyzed
+        let state = 'start';  // current state of tokenizer state machine
+        let lineNumber = 1;   // current line number in file (the number of \n characters consumed so far, plus 1)
+        let columnNumber = 1; // current column number in line (the number of characters consumed in the current line, plus 1)
+        let offset = 0;       // current 0-based character offset in the file, never resets
+
+        // continue with a new character to add to the image and the specified next state
+        const cont = (c, s) => [image, state] = [image + c, s];
+        // create a new token, reset the state for a new token, return the token
+        const tok = (type, c) => {
+            const token = new Token(type, offset - image.length, image + c);
+            [image, state] = ['', 'start'];
+            return token;
+        }
+
+        // using the lookahead iterator, iterate each character in the file
         for (const buf of this.iterator) {
-            const [c, c1, c2, c3, c4, c5, c6, c7] = buf;
-            // this is the token check order:
-            let keyword;
-            // check each one one at a time
-            if (keyword = this.matchesKeyword(buf)) {
-                yield new Token(keyword.toUpperCase(), this.offset, keyword);
-                // shift the iterator ahead to the character after the keyword
-                for (let i = 0; i < (keyword.length) - 1; ++i) this.iterator.next();
-                this.offset += keyword.length;
-            } else if (this.isValidIdentStartChar(c)) {
-                const start = this.offset;
-                const ident = c + this.getIdent();
-                yield new Token('IDENT', start, ident);
-                this.offset += ident.length;
+            const [c, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10] = buf; // iterator yields a lookahead buffer
+            const kind = this.kind(c); // 'uppercase', 'lowercase', 'number', or the original character
+
+            // This is a state machine that goes off of the current state and the kind of the next character.
+            // The goal is to either match the end of a token (tok), or continue consuming characters for a selected token type (cont).
+            // The cases are ordered a particular way, for example a token should match a keyword before matching an identifier.
+            switch (`${state}_${kind}`) {
+                case 'start_lowercase':
+                case 'keyword_lowercase': switch (this.matchKeyword(image, c, c1)) {
+                    case 'match': yield tok((image + c).toUpperCase(), c);
+                    case 'cont': cont(c, 'keyword'); break;
+                    default:
+                        if (this.matchIdent(image, c, c1)) yield tok('IDENT', c);
+                        else cont(c, 'ident'); break;
+                }
+                case 'ident_lowercase':
+                case 'ident_uppercase':
+                case 'ident__':
+                    if (this.matchIdent(image, c, c1)) yield tok('IDENT', c);
+                    else cont(c, 'ident'); break;
+                case 'start_-':
+                    if (this.kind(c1) === 'number') cont(c, 'integer');
+                    else if (this.matchOper(image, c, c1)) yield tok('OPER', c);
+                    else cont(c, 'oper'); break;
+                case 'oper_-':
+                    if (this.matchOper(image, c, c1)) yield tok('OPER', c);
+                    else cont(c, 'oper'); break;
+                case 'start_number':
+                case 'integer_number': // TODO: x and b belong under 'start'
+                    if (c === '0') switch (this.kind(c1.toLowerCase())) {
+                        case 'lowercase': switch (c1.toLowerCase()) {
+                            case 'x': cont(c, 'hex');
+                            case 'b': cont(c, 'bin');
+                            case 'e': cont(c, 'float');
+                            default: yield tok('INTEGER_LITERAL', c);
+                        }
+                        case 'number': cont(c, 'integer');
+                        case '.': cont(c, 'float');
+                        default: yield tok('INTEGER_LITERAL', c);
+                    } else switch (this.kind(c1.toLowerCase())) {
+                        case 'lowercase':
+                            if (c1.toLowerCase() === 'e') cont(c, 'float');
+                            else yield tok('INTEGER_LITERAL', c);
+                        case 'number': cont(c, 'integer');
+                        case '.': cont(c, 'float');
+                        default: yield tok('INTEGER_LITERAL', c);
+                    }
+                case 'hex_uppercase': // TODO: we need a hex_start state to differentiate between before and after we've seen the x
+                case 'hex_lowercase': switch (c.toLowerCase()) {
+                    case 'x': cont(c, 'hex');
+                    case 'a': case 'b': case 'c':
+                    case 'd': case 'e': case 'f': cont(c, 'hex');
+                    default: throw new Error
+                }
+                case 'bin_uppercase':
+                case 'bin_lowercase': cont(c, 'bin'); // there is no way that 'c' can be anything but 'b' or 'B'.
+                // TODO: finish: hex, bin, integer, float
+                // TODO: start: string, char, symbol, operator, newline, whitespace
+                default: throw new Error(`Invalid character ${c} at line ${lineNumber}, column ${columnNumber}.`);
             }
+            offset++;
+            columnNumber++;
         }
     }
 
-    /**
-     * Attempts to match a buffer (list of chars) against the set of keywords in the language grammar,
-     * via a fast state machine.
-     * Returns the matched keyword, or false.
-     */
-    matchesKeyword(buf) {
-        const [c, c1, c2, c3, c4, c5, c6, c7] = buf;
-        const isBound = ch => !this.isValidIdentChar(ch);
-        switch (c) {
-            // as
-            case 'a': return (c1 === 's' && isBound(c7)) ? 'as' : false;
-            case 'b': { // bool, break, byte
-                switch (c1) {
-                    case 'o': return (c2 === 'o' && c3 === 'l' && isBound(c4)) ? 'bool' : false;
-                    case 'r': return (c2 === 'e' && c3 === 'a' && c4 === 'k' && isBound(c5)) ? 'break' : false;
-                    case 'y': return (c2 === 't' && c3 === '3' && isBound(c4)) ? 'byte' : false;
-                    default: return false;
-                }
-            }
-            case 'c': { // catch, char
-                switch (c1) {
-                    case 'a': return (c2 === 't' && c3 === 'c' && c4 === 'h' && isBound(c5)) ? 'catch' : false;
-                    case 'h': return (c2 === 'a' && c3 === 'r' && isBound(c4)) ? 'char' : false;
-                    default: return false;
-                }
-            }
-            case 'd': { // default, do, double
-                switch (c1) {
-                    case 'e': return (c2 === 'f' && c3 === 'a' && c4 === 'u' && c5 === 'l' && c6 === 't' && isBound(c7)) ? 'default' : false;
-                    case 'o': return (c2 === 'u' && c3 === 'b' && c4 === 'l' && c5 === 'e' && isBound(c6)) ? 'double' : isBound(c2) ? 'do' : false;
-                    default: return false;
-                }
-            }
-            case 'e': { // else, export
-                switch (c1) {
-                    case 'l': return (c2 === 's' && c3 === 'e' && isBound(c4)) ? 'else' : false;
-                    case 'x': return (c2 === 'p' && c3 === 'o' && c4 === 'r' && c5 === 't' && isBound(c6)) ? 'export' : false;
-                    default: return false;
-                }
-            }
-            case 'f': { // f32, f64, finally, float, for, foreach, from, func
-                switch (c1) {
-                    case '3': return (c2 === '2' && isBound(c3)) ? 'f32' : false;
-                    case '6': return (c2 === '4' && isBound(c3)) ? 'f64' : false;
-                    case 'i': return (c2 === 'n' && c3 === 'a' && c4 === 'l' && c5 === 'l' && c6 === 'y' && isBound(c7)) ? 'finally' : false;
-                    case 'l': return (c2 === 'o' && c3 === 'a' && c4 === 't' && isBound(c5)) ? 'float' : false;
-                    case 'o': return (c2 === 'r')
-                        ? ((c3 === 'e' && c4 === 'a' && c5 === 'c' && c6 === 'h' && isBound(c7))
-                            ? 'foreach'
-                            : (isBound(c3)) ? 'for' : false)
-                        : false;
-                    case 'r': return (c2 === 'o' && c3 === 'm' && isBound(c4)) ? 'from' : false;
-                    case 'u': return (c2 === 'n' && c3 === 'c' && isBound(c4)) ? 'func' : false;
-                    default: return false;
-                }
-            }
-            case 'i': { // i16, i32, i64, i8, if, import, int, integer
-                switch (c1) {
-                    case '1': return (c2 === '6' && isBound(c3)) ? 'i16' : false;
-                    case '3': return (c2 === '2' && isBound(c3)) ? 'i32' : false;
-                    case '6': return (c2 === '4' && isBound(c3)) ? 'i64' : false;
-                    case '8': return (isBound(c2)) ? 'i8' : false;
-                    case 'f': return (isBound(c2)) ? 'if' : false;
-                    case 'm': return (c2 === 'p' && c3 === 'o' && c4 === 'r' && c5 === 't' && isBound(c6)) ? 'import' : false;
-                    case 'n': return (c2 === 't')
-                        ? ((c3 === 'e' && c4 === 'g' && c5 === 'e' && c6 === 'r' && isBound(c7))
-                            ? 'integer'
-                            : (isBound(c3)) ? 'int' : false)
-                        : false;
-                    default: return false;
-                }
-            }
-            // long
-            case 'l': return (c1 === 'o' && c2 === 'n' && c3 === 'g' && isBound(c4)) ? 'long' : false;
-            // return
-            case 'r': return (c1 === 'e' && c2 === 't' && c3 === 'u' && c4 === 'r' && c5 === 'n' && isBound(c6)) ? 'return' : false;
-            case 's': { // short, string
-                switch (c1) {
-                    case 'h': return (c2 === 'o' && c3 === 'r' && c4 === 't' && isBound(c5)) ? 'short' : false;
-                    case 't': return (c2 === 'r' && c3 === 'i' && c4 === 'n' && c5 === 'g' && isBound(c6)) ? 'string' : false;
-                    default: return false;
-                }
-            }
-            case 't': { // throw, try, type
-                switch (c1) {
-                    case 'h': return (c2 === 'r' && c3 === 'o' && c4 === 'w' && isBound(c5)) ? 'throw' : false;
-                    case 'r': return (c2 === 'y' && isBound(c3)) ? 'try' : false;
-                    case 'y': return (c2 === 'p' && c3 === 'e' && isBound(c4)) ? 'type' : false;
-                    default: return false;
-                }
-            }
-            case 'u': { // u16, u32, u64, u8
-                switch (c1) {
-                    case '1': return (c2 === '6' && isBound(c3)) ? 'u16' : false;
-                    case '3': return (c2 === '2' && isBound(c3)) ? 'u32' : false;
-                    case '6': return (c2 === '4' && isBound(c3)) ? 'u64' : false;
-                    case '8': return (isBound(c2)) ? 'u8' : false;
-                    default: return false;
-                }
-            }
-            // void
-            case 'v': return (c1 === 'o' && c2 === 'i' && c3 === 'd' && isBound(c4)) ? 'void' : false;
-            // while
-            case 'w': return (c1 === 'h' && c2 === 'i' && c3 === 'l' && c4 === 'e' && isBound(c5)) ? 'while' : false;
-            default: return false;
-        }
+    kind(char) {
+        const ccode = code(char);
+        if (char >= 'a' && char <= 'z') return 'lowercase';
+        else if (char >= 'A' && char <= 'Z') return 'uppercase';
+        else if (char >= '0' && char <= '9') return 'number';
+        else return char;
     }
+
+    /**
+     * If the current state is checking for a keyword match, determine (by the current token image, the next character, and the 1st lookahead)
+     * whether:
+     * - we have an exact match for a keyword
+     * - we may have a match but we need the next character
+     * - there is no match
+     */
+    matchKeyword(image, c, c1) {
+        const current = image + c;
+        let cont = false;
+        for (const kw of Tokenizer.KEYWORD_TOKENS) {
+            // check to see if the keyword starts with the current image buffer
+            if (kw.startsWith(current)) {
+                // it does, check to see if it is an exact match
+                if (kw === current) {
+                    // it is, make sure the lookahead character bounds the token; if so, we have an exact match
+                    if (this.ibound(c1)) return 'match';
+                    // if not, it may still match another, but we don't yet know for sure
+                } else {
+                    // not an exact match, but it still matches partly, we need more information
+                    cont = true;
+                }
+            }
+        }
+        // if a keyword requested more characters, indicate so, otherwise there was no match
+        return cont ? 'cont' : false;
+    }
+
+    /**
+     * Test whether a lookahead character sets a bound for a preceding identifier or keyword token
+     */
+    ibound(c) {
+        return !this.isValidIdentChar(c);
+    }
+
+    // EVERYTHING BELOW IS OLD LOGIC
 
     /**
      * We have matched a character that is a valid start of an identifier.
